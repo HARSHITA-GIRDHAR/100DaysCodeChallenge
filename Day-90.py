9240346d015bb5c8541c56897ca82788eaf90ed7
HARSHITA GIRDHAR <130740173+HARSHITA-GIRDHAR@users.noreply.github.com>
2024-04-08 18:00:42 +0530

    Add files via upload

---
+++ Day-90.py
@@ -0,0 +1,24 @@
+import turtle
+dist = 1
+flag = 500
+
+# initialising turtle
+spiral = turtle.Turtle()
+
+# changing speed of turtle
+spiral.speed(10)
+
+# making pattern
+while flag:
+
+	# makes the turtle to move forward
+	spiral.forward(dist)
+	
+	# makes the turtle to move left
+	spiral.left(120)
+	spiral.left(1)
+	dist += 1
+	flag -= 1
+
+turtle.done()
+
